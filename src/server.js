72907548505759aeac47f4887eea04b1d805a7a2
Vovk <vovkvaleria06@gmail.com>
2026-05-21 23:01:28 +0300

    finish task

--- src/server.js
+++ src/server.js
@@ -1,5 +1,65 @@
-// src/index.js
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
-const message = "Hello world";
+const app = express();
+const PORT = process.env.PORT ?? 3000;
 
-console.log(message);
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+//Реалізація маршрутів
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const noteId = Number(req.params.noteId);
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+// Штучна помилка
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+//Обробка неіснуючих маршрутів
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+//Обробка помилок
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd ? 'Internal Server Error' : err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
